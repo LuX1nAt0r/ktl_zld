2140c2b89ed7e678856608e46eefde2062fe8825
LuX1nAt0r <l.landsteiner@t-online.de>
2021-06-09 17:14:50 +0200

    Is now showing thirteen colors in a screen

--- src/main/kotlin/Window.kt
+++ src/main/kotlin/Window.kt
@@ -1,5 +1,8 @@
+import java.awt.Color
+import java.awt.Dimension
 import java.awt.EventQueue
 import javax.swing.*
+import javax.swing.SwingConstants.LEADING
 
 class Window(title: String) : JFrame(){
 
@@ -8,22 +11,86 @@ class Window(title: String) : JFrame(){
     }
 
     private fun createUI(title: String){
+
+        val stdCols = arrayOf<Color>(
+            Color.BLACK, Color.BLUE, Color.CYAN,
+            Color.DARK_GRAY, Color.GRAY, Color.GREEN,
+            Color.LIGHT_GRAY, Color.MAGENTA, Color.ORANGE,
+            Color.PINK, Color.RED, Color.WHITE, Color.YELLOW
+        )
+
+        val labels = stdCols.map {
+            JLabel("",null, LEADING).apply {
+                minimumSize = Dimension(90, 40)
+                background = it
+                isOpaque = true
+            }
+        }
+
+
+        createLayout(labels)
+
+
+
+
         setTitle(title)
 
         defaultCloseOperation= JFrame.EXIT_ON_CLOSE
-        setSize(300, 200)
         setLocationRelativeTo(null)
 
     }
+
+    private fun createLayout(labels: List<JLabel>){
+        val gl = GroupLayout(contentPane)
+        contentPane.layout = gl
+
+        gl.autoCreateContainerGaps = true
+        gl.autoCreateGaps = true
+
+        gl.setHorizontalGroup(gl.createParallelGroup()
+            .addGroup(gl.createSequentialGroup()
+                .addComponent(labels[0])
+                .addComponent(labels[1])
+                .addComponent(labels[2])
+                .addComponent(labels[3]))
+            .addGroup(gl.createSequentialGroup()
+                .addComponent(labels[4])
+                .addComponent(labels[5])
+                .addComponent(labels[6])
+                .addComponent(labels[7]))
+            .addGroup(gl.createSequentialGroup()
+                .addComponent(labels[8])
+                .addComponent(labels[9])
+                .addComponent(labels[10])
+                .addComponent(labels[11]))
+            .addComponent(labels[12])
+        )
+
+        gl.setVerticalGroup(gl.createSequentialGroup()
+            .addGroup(gl.createParallelGroup()
+                .addComponent(labels[0])
+                .addComponent(labels[1])
+                .addComponent(labels[2])
+                .addComponent(labels[3]))
+            .addGroup(gl.createParallelGroup()
+                .addComponent(labels[4])
+                .addComponent(labels[5])
+                .addComponent(labels[6])
+                .addComponent(labels[7]))
+            .addGroup(gl.createParallelGroup()
+                .addComponent(labels[8])
+                .addComponent(labels[9])
+                .addComponent(labels[10])
+                .addComponent(labels[11]))
+            .addComponent(labels[12])
+        )
+        pack()
+
+    }
+
+
+
 }
 
 
 
-private fun createAndShowGUI(){
-    val frame = Window("Simple")
-    frame.isVisible = true
-}
-
-fun main(args: Array<String>) {
-    EventQueue.invokeLater(::createAndShowGUI)
-}
--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,7 +1,16 @@
+import java.awt.EventQueue
 
 
 
+fun main(args: Array<String>) {
+    EventQueue.invokeLater(::createAndShowGUI)
+}
 
+
+private fun createAndShowGUI(){
+    val frame = Window("Simple")
+    frame.isVisible = true
+}
 
 
 
